87da24bcd4e32006206e59f259e5e22b29424181
Paras gupta <43930565+parasgupta0018@users.noreply.github.com>
2021-07-31 22:43:47 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,21 +6,16 @@ const bcrypt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('../validation');
 const { valid } = require('joi');
 
-// register route
 router.post('/register', async (req, res) => {
-  // validate data
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  // check if the user is already in the db
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send('Email already exist');
 
-  // hash passwords
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-  // create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
@@ -35,21 +30,16 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// login route
 router.post('/login', async (req, res) => {
-  // validate data
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  // check if the email exists
   const user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send('Email is not found');
 
-  // password is correct
   const validPass = await bcrypt.compare(req.body.password, user.password);
   if (!validPass) return res.status(400).send('Invalid password');
 
-  // create and assign a token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
   res.header('auth-token', token).json({id: user._id, email: user.email, name : user.name, token});
 });
